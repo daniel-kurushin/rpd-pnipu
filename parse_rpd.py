625c76a81e06b8cc7ff71fa0f6a53d969e1c22ab
Курушин Даниил <daniel.kurushin@gmail.com>
2017-10-23 20:41:06 +0500

    fodt functions -> docx

--- parse_rpd.py
+++ parse_rpd.py
@@ -2,12 +2,48 @@ import sys
 import re
 
 from docx import Document
+from bs4 import BeautifulSoup as BS
 from json import dumps
 
 class RPD():
+	content = {}
+
+	def __safe_get_text(self, element):
+		try:
+			return element.get_text().strip().lower()
+		except:
+			return ""
+
+	def факультет(self, element):
+		return {'факультет':self.__safe_get_text(element.parent.parent.find_all('w:tc')[1])}
+
+	def кафедра(self, element):
+		return {"кафедра":None}
+
+	def проректор(self, element):
+		return {"проректор по учебной работе":None}
+
+	def умкд(self, element):
+		return {"учебно-методический комплекс дисциплины":None}
+
 
 	def parse(self):
+		self.soup = BS(self.docx.element.xml)
+		with open('log', 'w') as f:
+			f.write(self.soup.prettify())
+			f.close()
+		for p in self.soup.find_all('w:p'):
+			t = self.__safe_get_text(p)
+			if t == "факультет":
+				self.content.update(self.факультет(p))
+			if t == "кафедра":
+				self.content.update(self.кафедра(p))
+			if t == "проректор по учебной работе":
+				self.content.update(self.проректор(p))
+			if t == "учебно-методический комплекс дисциплины":
+				self.content.update(self.умкд(p))
 
+		print(self.content)
 
 	def __init__(self, docpath = "", content = {}):
 		try:
@@ -16,7 +52,7 @@ class RPD():
 			self.docpath = docpath
 			self.parse()
 		except FileNotFoundError:
-			docx = Document()
+			self.docx = Document()
 		self.content = content
 
 def test():
