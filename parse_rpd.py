a28f90fd180da13cb6f5edfb4a144a2c241124a3
Курушин Даниил <daniel.kurushin@gmail.com>
2017-10-25 18:31:16 +0500

    authors

--- parse_rpd.py
+++ parse_rpd.py
@@ -8,6 +8,7 @@ from json import dumps
 from compare import ngramm_compare as NC
 from compare import ngramm_compare_phrase as NCP
 from compare import MIN_W, MIN_P
+from pymystem3 import Mystem
 
 class RPD():
 	content = {}
@@ -57,8 +58,6 @@ class RPD():
 				text += re.sub(r"[\n\r\t]+", " ", element.get_text()).strip()
 			except AttributeError:
 				pass
-		with open('/tmp/a', 'w') as f:
-			f.write(text)
 		return {
 			"учебно-методический комплекс дисциплины":
 			re.findall(r'[«"](.*)[»"]', text)[0].strip()
@@ -135,7 +134,76 @@ class RPD():
 		except AssertionError:
 			return {"виды контроля": n}
 
+	def разработчик(self, element):
+		def get_sokr(author_str):
+			return re.findall(r"(\w\.\s*\w\.)", author_str.split('_')[-1])[0]
 
+		def get_surname(initials, author_str):
+			rez = re.findall(r".*[ _](.*) (%s)(.*).*" % initials, author_str)[0]
+			return rez[0] if rez[0] != "" else rez[2]
+
+		def get_grade(author_str):
+			try:
+				rez = re.findall(r"([^.]{2,7}\.[^.]{2,7}\.[^.]{2,7})[, ]", author_str)[0]
+			except IndexError:
+				try:
+					rez = re.findall(r"(\w\.\w\.\w\.)", author_str)[0]
+				except IndexError:
+					raise ValueError
+			return rez.strip(',')
+
+		def get_position(grade, author_str):
+			# доц.,  к.т.н., доц. _________________ курушин
+			rez = re.findall(r"(.+?)?,?(%s), ?(.+?)? " % grade, author_str)[0]
+			return rez[0] if rez[0] != "" else rez[2]
+
+		def filter_sokr(initials):
+			rez = tuple([s.strip() for s in initials.split(".")[0:2]])
+			return ("%s. %s." % rez).upper()
+
+		def filter_surname(surname):
+			return surname.strip().capitalize()
+
+		def filter_grade(grade):
+			return re.sub(r"\.", ". ", re.sub(r"[, ]", "", grade)).strip()
+
+		def filter_position(position):
+			return re.sub(r"[, ]", "", position)
+
+		n = 0
+		text = ""
+		while n < 20:
+			n += 1
+			element = element.nextSibling
+			_ = self.__safe_get_text(element)
+			if NCP("Рабочая программа рассмотрена и одобрена на заседании кафедры", _) > MIN_P:
+				break
+			text += "^" + _
+		text = re.sub(r"\^+","^", text)
+		rez = [_.strip() for _ in text.split("^") if _ != ""]
+		authors = []
+		for author in rez:
+			try:
+				initials = get_sokr(author)
+				surname = get_surname(initials, author)
+				grade = get_grade(author)
+				position = get_position(grade, author)
+				initials = filter_sokr(initials)
+				surname = filter_surname(surname)
+				grade = filter_grade(grade)
+				position = filter_position(position)
+				authors += [
+					{
+						"ФИО": "%s %s" % (initials, surname),
+						"Должность": position,
+						"Степень": grade,
+					}
+				]
+			except Exception as e:
+				pass
+
+		return { "авторы" : authors }
+		
 	def parse(self):
 		self.soup = BS(self.docx.element.xml)
 		with open('log', 'w') as f:
@@ -171,11 +239,13 @@ class RPD():
 				self.content.update(self.трудоёмкость(p))
 			if self.__is_not_set("виды контроля") and NCP(t, "виды контроля") > MIN_P:
 				self.content.update(self.виды_контроля(p))
-
+			if self.__is_not_set("разработчик(-и)") and NC(t, "разработчик(-и)") > MIN_W:
+				self.content.update(self.разработчик(p))
 
 		print(dumps(self.content, indent = 4, ensure_ascii = 0))
 
 	def __init__(self, docpath = "", content = {}):
+		self.ma = Mystem()
 		try:
 			open(docpath)
 			self.docx = Document(docpath)
@@ -186,7 +256,7 @@ class RPD():
 		self.content = content
 
 def test():
-	rpd = RPD('РПД_дисциплины.docx')
+	rpd = RPD('РПД_дисциплины_1.docx')
 	del rpd
 	rpd = RPD()
 
