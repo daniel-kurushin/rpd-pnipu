9f54cf4f4ec1a23016be119f0dc9e84c34ce75bd
Курушин Даниил <daniel.kurushin@gmail.com>
2017-11-03 11:10:35 +0500

    static

--- server.py
+++ server.py
@@ -7,7 +7,47 @@ from time import sleep
 from datetime import datetime
 
 class RPDRequestHandler(BaseHTTPRequestHandler):
-	pass
+	def _load_file(self, name, context=None, content_type='text/html'):
+		self.send_response(200)
+		self.send_header('Content-type', content_type)
+		self.end_headers()
+		data = ''
+		with open(name, 'rb') as _file:
+			data = _file.read()
+		if context:
+			for key in context:
+				data = data.replace(
+					bytes(key, 'utf-8'),
+					bytes(context[key], 'utf-8')
+				)
+		self.wfile.write(data)
+
+	def _load_str(self, data):
+		self.send_response(200)
+		self.end_headers()
+		self.wfile.write(bytes(data, 'utf-8'))
+
+	def _to_json(self, data):
+		self.send_response(200)
+		self.send_header('Content-type', 'application/json')
+		self.end_headers()
+		self.wfile.write(str.encode(json.dumps(data, indent = 4)))
+
+	def do_GET(self):
+		if self.path.endswith('png'):
+			self._load_file(self.path.lstrip('/'), content_type='image/png')
+		elif self.path.endswith('jpg'):
+			self._load_file(self.path.lstrip('/'), content_type='image/jpeg')
+		elif self.path.startswith('/static'):
+			content_type = 'text/html'
+			if self.path.endswith('jpg'):
+				content_type = 'image/jpeg'
+			elif self.path.endswith('css'):
+				content_type = 'text/css'
+			self._load_file(self.path.lstrip('/'), content_type=content_type)
+		else:
+			self._load_file('index.html')
+
 
 if __name__ == '__main__':
 	server = HTTPServer(('0.0.0.0', 8000), RPDRequestHandler)
