16de3f7775928966bb2ecbf442bc38c959f35c36
Курушин Даниил <daniel.kurushin@gmail.com>
2017-11-03 10:55:05 +0500

    server

---
+++ server.py
@@ -0,0 +1,14 @@
+import json
+import sys
+
+from http.server import HTTPServer, BaseHTTPRequestHandler, urllib
+from threading import Thread
+from time import sleep
+from datetime import datetime
+
+class RPDRequestHandler(BaseHTTPRequestHandler):
+	pass
+
+if __name__ == '__main__':
+	server = HTTPServer(('0.0.0.0', 8000), IISURequestHandler)
+	server.serve_forever()
