edf4b66a7f71ab2dcce1e2af2c69521952bc637f
Курушин Даниил <daniel.kurushin@gmail.com>
2017-11-07 14:21:51 +0500

    improve indexing

--- database/subjects.py
+++ database/subjects.py
@@ -1,31 +1,39 @@
 from json import load
 # from lingv.index import index
 from pymystem3 import Mystem
-from rutermextract import TermExtractor as TE
 
-def index(text, mystem = Mystem(), term_extractor = TE()):
+def index(text, mystem = Mystem()):
 	stop_words = [
 		"факультет",
 		"кафедра",
+		"и",
+		"пример",
+		"на",
+		"в",
+		"ее",
+		"глава",
+		"из",
+		"средство",
+		"основа",
+		"учение",
+		"введение",
 	]
-	terms = term_extractor(text)
 	idx = {}
 
-	for term in terms:
-		for rez in mystem.analyze(str(term)):
-			try:
-				word   = rez['analysis'][0]['lex']
-				weight = (term.count / len(terms))
-				weight = weight / 10 if word in stop_words else weight
-				idx.update({word:weight})
-			except:
-				pass
+	analysis = mystem.analyze(text)
+	for rez in analysis:
+		try:
+			word   = rez['analysis'][0]['lex']
+			weight = (1 / len(analysis))
+			weight = weight / 10 if word in stop_words else weight
+			idx.update({word:weight})
+		except:
+			pass
 
 	return idx
 
 def index_subjects():
 	mystem = Mystem()
-	term_extractor = TE()
 	subjects = load(open('database/subjects.json'))
 	structur = load(open('database/structure.json'))
 
@@ -51,7 +59,7 @@ def index_subjects():
 					short = subjects[faculty][specialisation][subject]['кафедра'].replace('и','')
 					department, head = find_department(short)
 					text = "%s %s %s %s %s %s" % (faculty, specialisation, subject, department, short, head)
-					idx = index(text, mystem, term_extractor)
+					idx = index(text, mystem)
 					rez.update({subject: idx})
 				except Exception as e:
 					print(e, faculty, specialisation, subject, subjects[faculty][specialisation][subject])
