d5ae594a3d0f41de0208015bf30923281b06ac45
mforv <master-forve@ya.ru>
2018-01-09 22:08:03 +0500

    Explicit Algorithm Realization

--- gen_rpd.py
+++ gen_rpd.py
@@ -12,12 +12,36 @@ from bs4 import BeautifulSoup as BS
 from compare import ngramm_compare as NC
 from compare import ngramm_compare_phrase as NCP
 from compare import MIN_W, MIN_P
-#from pymystem3 import Mystem
+# from pymystem3 import Mystem
 
-sections_order = ("факультет", 'кафедра', "проректор по учебной работе", 'учебно-методический комплекс дисциплины', 'программа', "направление подготовки", "профиль программы магистратуры", "квалификация выпускника", "выпускающая кафедра", "форма обучения", "курс", "семестр(-ы)", "трудоёмкость", "виды контроля", "авторы", "PAGE_BREAK", "утверждение и согласование", "цель дисциплины", "компетенции", "результаты обучения")
+sections_order = ("факультет", 'кафедра', "проректор по учебной работе", 'учебно-методический комплекс дисциплины',
+                  'программа', "направление подготовки", "профиль программы магистратуры", "квалификация выпускника",
+                  "выпускающая кафедра", "форма обучения", "курс", "семестр(-ы)", "трудоёмкость", "виды контроля",
+                  "авторы", "PAGE_BREAK", "утверждение и согласование", "цель дисциплины", "компетенции",
+                  "результаты обучения")
+
+
+def load_blocks(input_file='./rpd.json'):
+    """Загружает выделенные парсером признакосодержащие блоки (ПСБ)"""
+    return json.load(open(input_file, 'r', encoding='utf-8'))
+
+
+def get_clear_blocks(raw_blocks, stored_blocks_db='./rpd_db.json'):
+    """Приводит полученные входные значения ПСБ raw_blocks к сохранненому эталону из stored_blocks_db"""
+    cleared_blocks = {}
+    with json.load(open(stored_blocks_db, 'r', encoding='utf-8')) as db:
+        for block_name in raw_blocks.keys():
+            for clear_name in db.keys():
+                if 1.0 <= NCP(block_name, clear_name) <= 1.5:
+                    # в настоящее время эта часть реализована исключительно в тестовом режиме
+                    # в дальнейшем необходим синтез двух вариантов значения — из базы и из входящих данных
+                    if 1.0 <= NCP(raw_blocks[block_name], db[clear_name]) <= 1.5:
+                        cleared_blocks[clear_name] = db.keys[clear_name]
+    return cleared_blocks
 
 
 def process_types(element):
+    """Обрабатывает элементы разных типов для их включения в текст"""
     out = ''
     if isinstance(element, str):
         out = ' ' + element + '\n'
@@ -30,25 +54,28 @@ def process_types(element):
         for key in element.keys():
             out += key
             out += process_types(element[key])
+    else:
+        out = ' ' + str(element) + '\n'
     return out
 
 
-def gen(input_file = './rpd.json'):
+def generate_text(source_blocks):
+    """Генерирует текст из очищенных признакосодержащих блоков"""
     output_string = ''
-    file_data = json.load(open(input_file, 'r', encoding='utf-8'))
     for section in sections_order:
         if section != 'PAGE_BREAK':
-            for key in file_data.keys():
+            for key in source_blocks.keys():
                 distance = NCP(section, key)
-                if distance >= 1.0 and distance <= 1.5:
+                if 1.0 <= distance <= 1.5:
                     output_string += key + ':'
-                    output_string += process_types(file_data[key])
+                    output_string += process_types(source_blocks[key])
         else:
             output_string += section + '\n'
     return output_string
 
 
-def create_docx(rpd_string='', file_name='rpd.docx'):
+def build_rpd_docx(rpd_string='', file_name='rpd.docx'):
+    """Собирает DOCX-файл РПД file_name из строкового представления РПД rpd_string"""
     rpd_doc = Document()
     rpd_data = rpd_string.split('\n')
     for line in rpd_data:
@@ -60,4 +87,6 @@ def create_docx(rpd_string='', file_name='rpd.docx'):
 
 
 if __name__ == '__main__':
-    create_docx(gen())
+    # current_data_blocks = get_clear_blocks(load_blocks())
+    current_data_blocks = load_blocks()
+    build_rpd_docx(generate_text(current_data_blocks))
