7bcfece5b7c3325c405b9bacda48395cac5a2a77
mforv <master-forve@ya.ru>
2018-02-18 21:41:53 +0500

    Template formatting and DOCX generator update

--- gen_rpd.py
+++ gen_rpd.py
@@ -1,62 +1,18 @@
 #!usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import sys
 import re
-import math
 import json
-import os
-
-from docx import Document
-from bs4 import BeautifulSoup as BS
 
 from compare import ngramm_compare as NC
 from compare import ngramm_compare_phrase as NCP
-from compare import MIN_W, MIN_P
-# from pymystem3 import Mystem
 
-sections_order = ("факультет", 'кафедра', "проректор по учебной работе", 'учебно-методический комплекс дисциплины',
-                  'программа', "направление подготовки", "профиль программы магистратуры", "квалификация выпускника",
-                  "выпускающая кафедра", "форма обучения", "курс", "семестр(-ы)", "трудоёмкость", "виды контроля",
-                  "авторы", "PAGE_BREAK", "утверждение и согласование", "цель дисциплины", "компетенции",
-                  "результаты обучения")
+from docxtpl import DocxTemplate, RichText
 
 
 def load_blocks(input_file='./rpd.json'):
     """Загружает выделенные парсером признакосодержащие блоки (ПСБ)"""
     return json.load(open(input_file, 'r', encoding='utf-8'))
-
-
-def get_clear_types(raw_element, stored_element):
-    """Обрабатывает каждый тип сырых данных с попыткой приведения его к сохраненному эталону"""
-    # тут пока что принимается на доверие, что типы сырого и сохраненного элемента совпадают, но это только пока
-    if isinstance(raw_element, str):
-        if 1.0 <= NCP(raw_element, stored_element) <= 1.5:
-            return stored_element
-        else:  # возможно, имеет смысл поменять просто на else
-            return raw_element
-    elif isinstance(raw_element, list):
-        clear_list = []
-        for i in range(0, len(raw_element)):
-            if i < len(stored_element):
-                clear_list.append(get_clear_types(raw_element[i], stored_element[i]))
-            elif len(stored_element) > i:
-                clear_list.append(stored_element[i:])
-            else:
-                clear_list.append(raw_element[i])
-        return clear_list
-    elif isinstance(raw_element, dict):
-        clear_dict = {}
-        for key in raw_element.keys():
-            for cl_key in stored_element.keys():
-                if 1.0 <= NCP(key, cl_key) <= 1.5:
-                    clear_dict[cl_key] = get_clear_types(raw_element[key], stored_element[cl_key])
-                else:
-                    clear_dict[key] = get_clear_types(raw_element[key], stored_element[cl_key])
-            else:
-                clear_dict[key] = raw_element[key]
-    else:
-        return raw_element
 
 
 def get_clear_blocks(input_file='rpd.json'):
@@ -68,7 +24,10 @@ def get_clear_blocks(input_file='rpd.json'):
         for raw_name in raw_blocks.keys():
             for clear_name in clear_blocks.keys():
                 if 1.0 <= NCP(raw_name, clear_name) <= 1.5:
-                    cleared_blocks[clear_name] = get_clear_types(raw_blocks[raw_name], clear_blocks[clear_name])
+                    if 1.0 <= NCP(str(raw_blocks[raw_name]), str(clear_blocks[clear_name])) <= 1.5:
+                        cleared_blocks[clear_name] = clear_blocks[clear_name]
+                    else:
+                        cleared_blocks[clear_name] = raw_blocks[raw_name]
         return cleared_blocks
     except FileNotFoundError:
         print('No stored RPD template found, returning as is')
@@ -77,52 +36,68 @@ def get_clear_blocks(input_file='rpd.json'):
 
 def process_types(element):
     """Обрабатывает элементы разных типов для их включения в текст"""
-    out = ''
     if isinstance(element, str):
-        out = ' ' + element + '\n'
+        out = re.sub(r'[\[\]{}\'\"]', '', str(element))
     elif isinstance(element, list):
-        out += '\n'
-        for child_element in element:
-            out += process_types(child_element)
+        out = []
+        for el in element:
+            out.append(process_types(el))
     elif isinstance(element, dict):
-        out += '\n'
-        for key in element.keys():
-            out += key
-            out += process_types(element[key])
+        preformatted = []
+        for child_key in element.keys():
+            if isinstance(element[child_key], list):
+                preformatted.append({'key': child_key, 'cols': element[child_key]})
+            elif isinstance(element[child_key], dict):
+                preformatted.append({'key': child_key, 'cols': process_types(element[child_key])})
+            else:
+                preformatted.append({'key': child_key, 'cols': [process_types(element[child_key])]})
+        out = preformatted
     else:
-        out = ' ' + str(element) + '\n'
+        out = re.sub(r'[\[\]{}\'\"]', '', str(element))
     return out
 
 
-def generate_text(source_blocks):
-    """Генерирует текст из очищенных признакосодержащих блоков"""
-    output_string = ''
-    for section in sections_order:
-        if section != 'PAGE_BREAK':
-            for key in source_blocks.keys():
-                distance = NCP(section, key)
-                if 1.0 <= distance <= 1.5:
-                    output_string += key + ':'
-                    output_string += process_types(source_blocks[key])
-        else:
-            output_string += section + '\n'
-    return output_string
+def generate_context(source_blocks):
+    """Генерирует контекст для шаблона из очищенных признакосодержащих блоков"""
+    output_context = {}
+    for key in source_blocks.keys():
+        safe_key = re.sub(r'[-\s]', '_', re.sub(r'[()]', '', str(key)))
+        # print(safe_key)
+        output_context[safe_key] = process_types(source_blocks[key])
+    return output_context
 
 
-def build_rpd_docx(rpd_string='', file_name='rpd.docx'):
-    """Собирает DOCX-файл РПД file_name из строкового представления РПД rpd_string"""
-    rpd_doc = Document()
-    rpd_data = rpd_string.split('\n')
-    for line in rpd_data:
-        if line == 'PAGE_BREAK':
-            rpd_doc.add_page_break()
-        else:
-            rpd_doc.add_paragraph(line)
-    rpd_doc.save(file_name)
+def add_sub_docx(sub_docx):
+    document = DocxTemplate(sub_docx)
+    xml = re.sub(r'</?w:body[^>]*>', '', document.get_xml())
+    return xml
+
+
+def build_rpd_docx(source_blocks, template='rpd_template.docx', file_name='rpd_new.docx'):
+    """Собирает DOCX-файл РПД file_name из контекста input_context и шаблона template"""
+    template = DocxTemplate(template)
+    contents = add_sub_docx('rpd_contents.docx')
+    fos = add_sub_docx('rpd_fos.docx')
+    soft = add_sub_docx('rpd_soft.docx')
+    iss = add_sub_docx('rpd_soft.docx')
+    labs = add_sub_docx('rpd_labs.docx')
+    tools = add_sub_docx('rpd_tools.docx')
+    context = generate_context(source_blocks)
+    # print(context)
+    context['contents'] = contents
+    context['fos'] = fos
+    context['soft'] = soft
+    context['iss'] = iss
+    context['labs'] = labs
+    context['tools'] = tools
+    template.render(context)
+    name = source_blocks["учебно-методический комплекс дисциплины"]+"_"+source_blocks["направление подготовки"]["шифр"]+".docx"
+    template.save('gen/'+re.sub(r'\s', '_', name))
+    return re.sub(r'\s', '_', name)
 
 
 if __name__ == '__main__':
     # current_data_blocks = get_clear_blocks('rpd_0.json')
     # print(current_data_blocks)
     current_data_blocks = load_blocks()
-    build_rpd_docx(generate_text(current_data_blocks))
+    build_rpd_docx(current_data_blocks)
--- server.py
+++ server.py
@@ -21,6 +21,8 @@ from forms.lostform import LostForm
 from forms.searchform import SearchForm
 from forms.editorform import EditorForm
 
+import gen_rpd
+
 
 class RPDRequestHandler(BaseHTTPRequestHandler):
     def _get_cookies(self):
@@ -75,11 +77,11 @@ class RPDRequestHandler(BaseHTTPRequestHandler):
         print(rez)
         return rez
 
-    def _send_cookies(self, cookies = {}):
+    def _send_cookies(self, cookies={}):
         for cookie in cookies.keys():
             self.send_header('Set-Cookie', "%s=%s" % (cookie, urllib.parse.quote_plus(cookies[cookie])))
 
-    def _load_file(self, name, context=None, content_type='text/html', cookies = {}):
+    def _load_file(self, name, context=None, content_type='text/html', cookies={}):
         self.send_response(200)
         self._send_cookies(cookies)
         self.send_header('Content-type', content_type)
@@ -95,7 +97,7 @@ class RPDRequestHandler(BaseHTTPRequestHandler):
                 )
         self.wfile.write(data)
 
-    def _redirect(self, url, cookies = {}):
+    def _redirect(self, url, cookies={}):
         """редирект клиента с опциональным выставлением куков"""
         self.send_response(302)
         self.send_header('Location', url)
@@ -103,7 +105,7 @@ class RPDRequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(bytes('data', 'utf-8'))
 
-    def _load_str(self, data, cookies = {}):
+    def _load_str(self, data, cookies={}):
         self.send_response(200)
         self._send_cookies(cookies)
         self.end_headers()
@@ -113,9 +115,9 @@ class RPDRequestHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
-        self.wfile.write(str.encode(json.dumps(data, indent = 4)))
+        self.wfile.write(str.encode(json.dumps(data, indent=4)))
 
-    def _do_login(self, login = '', password = ''):
+    def _do_login(self, login='', password=''):
         try:
             _user = login
             _pass = password
@@ -124,15 +126,15 @@ class RPDRequestHandler(BaseHTTPRequestHandler):
                 raise WrongPasswordError('Неверный пароль для пользователя "%s"' % _user)
         except KeyError:
             raise WrongUsernameError('Нет пользователя с именем "%s"' % _user)
-        self._redirect('/rpd_main/?%s' % urllib.parse.urlencode({'login': _user}), cookies = set_auth_cookies(_user))
+        self._redirect('/rpd_main/?%s' % urllib.parse.urlencode({'login': _user}), cookies=set_auth_cookies(_user))
 
-    def show_login_form(self, login = None, password = None, redirect = None, error = None):
+    def show_login_form(self, login=None, password=None, redirect=None, error=None):
         self._load_str(
             LoginForm(
-                login = login,
-                password = password,
-                redirect = redirect,
-                error = error
+                login=login,
+                password=password,
+                redirect=redirect,
+                error=error
             )
         )
 
@@ -141,17 +143,17 @@ class RPDRequestHandler(BaseHTTPRequestHandler):
             LostForm()
         )
 
-    def show_confirm_form(self, _forward = None, _return = None):
+    def show_confirm_form(self, _forward=None, _return=None):
         self._load_str(
-            ConfirmExitFrom(_forward = _forward, _return = _return)
+            ConfirmExitFrom(_forward=_forward, _return=_return)
         )
 
-    def show_search_form(self, params = {}):
+    def show_search_form(self, params={}):
         self._load_str(
             SearchForm(params)
         )
 
-    def show_editor_form(self, params = {}):
+    def show_editor_form(self, params={}):
         self._load_str(
             EditorForm(params)
         )
@@ -163,8 +165,10 @@ class RPDRequestHandler(BaseHTTPRequestHandler):
             self._load_file(self.path.lstrip('/'), content_type='image/png')
         elif self.path.endswith('jpg'):
             self._load_file(self.path.lstrip('/'), content_type='image/jpeg')
-        elif self.path.startswith('/static'):
+        elif self.path.startswith('/static') or self.path.startswith('/gen'):
             content_type = 'text/html'
+            if self.path.endswith('docx'):
+                content_type = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
             if self.path.endswith('jpg'):
                 content_type = 'image/jpeg'
             elif self.path.endswith('css'):
@@ -182,9 +186,9 @@ class RPDRequestHandler(BaseHTTPRequestHandler):
             self.show_editor_form(params)
         elif params['is_auth'] and self.path.startswith('/logout'):
             if self.path.endswith('yes'):
-                self._redirect('/auth/', cookies = del_auth_cookies(params['login']))
+                self._redirect('/auth/', cookies=del_auth_cookies(params['login']))
             else:
-                self.show_confirm_form(_forward = '/logout/yes', _return = params['referer'])
+                self.show_confirm_form(_forward='/logout/yes', _return=params['referer'])
         else:
             self._redirect('/auth/')
 
@@ -203,6 +207,8 @@ class RPDRequestHandler(BaseHTTPRequestHandler):
                             f.write(raw_data)
                     except IOError:
                         print("No such dir")
+                if self.headers.get('docx'):
+                    gen_rpd.build_rpd_docx(gen_rpd.load_blocks())
                 self.send_response(200)
                 self.end_headers()
             except Exception as e:
@@ -214,9 +220,9 @@ class RPDRequestHandler(BaseHTTPRequestHandler):
                 _pass = data['pass'][0]
                 self._do_login(_user, _pass)
             except LoginError as e:
-                self.show_login_form(login = _user, password = _pass, error = e)
+                self.show_login_form(login=_user, password=_pass, error=e)
             except KeyError as e:
-                self.show_login_form(error = e)
+                self.show_login_form(error=e)
 
 
 if __name__ == '__main__':
